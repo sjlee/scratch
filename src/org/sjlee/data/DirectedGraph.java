319172b18258fe0fd1510299af713d414ddcc81c
Sangjin Lee <sjlee0@gmail.com>
2011-11-26 13:17:29 -0800

    implemented a path search (depth-first).

--- src/org/sjlee/data/DirectedGraph.java
+++ src/org/sjlee/data/DirectedGraph.java
@@ -65,6 +65,7 @@ public class DirectedGraph {
 					}
 				}
 			}
+			// iterate
 			try {
 				current = stack.pop(); // pop a vertex; if it is empty we'll handle it as an exception
 			} catch (EmptyStackException e) {
@@ -72,6 +73,72 @@ public class DirectedGraph {
 			}
 		} while (true);
 		System.out.println("depth-first traversal complete.");
+	}
+	
+	public DoublyLinkedList<Vertex> findPathDF(int startIndex, int endIndex) {
+		Stack<Vertex> stack = new Stack<Vertex>(); // stack for DFS
+		DoublyLinkedList<Vertex> path = new DoublyLinkedList<Vertex>(); // DLL for the path
+		Vertex current = vertices[startIndex];
+		int index;
+		do {
+			index = current.getIndex();
+			addToPath(current, path);
+			current.setVisited();
+			// see if we reached the end
+			if (current.getIndex() == endIndex) {
+				// we're done
+				break;
+			}
+			// push the neighbors
+			for (int i = 0; i < vertices.length; i++) {
+				if (edges[index][i] != 0) {
+					Vertex neighbor = vertices[i];
+					if (!neighbor.isVisited()) {
+						stack.push(neighbor);
+					}
+				}
+			}
+			// iterate
+			try {
+				current = stack.pop(); // completed the traversal
+			} catch (EmptyStackException e) {
+				current = null;
+				break;
+			}
+		} while (true);
+		// final operations
+		if (current == null) {
+			return null; // no path found
+		}
+		editPath(path, startIndex);
+		return path;
+	}
+	
+	private void addToPath(Vertex v, DoublyLinkedList<Vertex> path) {
+		path.add(v);
+		
+	}
+	
+	private void editPath(DoublyLinkedList<Vertex> path, int startIndex) {
+		DoublyLinkedNode<Vertex> current = path.getTail();
+		DoublyLinkedNode<Vertex> previous = current.prev;
+		while (previous != null && previous.value.index != startIndex) {
+			while (previous != null && edges[previous.value.index][current.value.index] != 0) {
+				current = previous;
+				previous = current.prev;
+			}
+			// there is no edge between the current and the previous: cycle
+			if (previous != null && previous.value.index != startIndex) {
+				while (previous != null && edges[previous.value.index][current.value.index] == 0) {
+					previous = previous.prev;
+				}
+				// fix
+				if (previous != null) {
+					previous.next = current;
+					current.prev = previous;
+				}
+			}
+		}
 	}
 	
 	public void resetVisited() {
@@ -146,5 +213,8 @@ public class DirectedGraph {
 		graph.traverseBF();
 		graph.resetVisited();
 		graph.traverseDF();
+		graph.resetVisited();
+		DoublyLinkedList<Vertex> path = graph.findPathDF(1, 3);
+		System.out.println(path);
 	}
 }
