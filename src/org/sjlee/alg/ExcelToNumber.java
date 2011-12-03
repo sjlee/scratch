169c08811d358fdaee9811713693af66051c6253
Sangjin Lee <sjlee0@gmail.com>
2011-12-03 15:25:14 -0800

    adding the other transformation.

--- src/org/sjlee/alg/ExcelToNumber.java
+++ src/org/sjlee/alg/ExcelToNumber.java
@@ -1,5 +1,6 @@
 package org.sjlee.alg;
 
+
 public class ExcelToNumber {
 	// a -> 1, b -> 2, ... aa -> 27, ...
 	public static final int UNIT = 26;
@@ -10,13 +11,35 @@ public class ExcelToNumber {
 		int value = 0;
 		for (int i = size-1; i >= 0; i--) {
 			char c = s.charAt(i);
-			value += getUnitValue(c)*((int)Math.pow(UNIT, (size - 1 - i)));
+			value += charToInt(c)*((int)Math.pow(UNIT, (size - 1 - i)));
 		}
 		return value;
 	}
 	
-	public static int getUnitValue(char c) {
+	public static int charToInt(char c) {
 		return c - 'A' + 1;
+	}
+	
+	public static String numberToExcel(int n) {
+		int current = n;
+		StringBuilder sb = new StringBuilder();
+		while (true) {
+			int quotient = current / UNIT;
+			int remainder = current % UNIT;
+			sb.append(intToChar(remainder));
+			if (quotient == 0) {
+				break;
+			}
+			current = quotient;
+		}
+		return sb.reverse().toString();
+	}
+	
+	/**
+	 * Ranges from 1 to 26.
+	 */
+	public static char intToChar(int i) {
+		return (char)(i + 'A' - 1);
 	}
 	
 	public static void main(String[] args) {
@@ -24,5 +47,9 @@ public class ExcelToNumber {
 		System.out.println(excelToNumber("AA"));
 		System.out.println(excelToNumber("AB"));
 		System.out.println(excelToNumber("BA"));
+		System.out.println(numberToExcel(1));
+		System.out.println(numberToExcel(27));
+		System.out.println(numberToExcel(28));
+		System.out.println(numberToExcel(53));
 	}
 }
